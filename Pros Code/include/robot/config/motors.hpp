55e01fa8cb22dea19f8eca9aa48b25b99aa6eddb
swordsparring <swordsparring@gmail.com>
2019-03-06 17:39:41 -0700

    start of pros code

---
+++ Pros Code/include/robot/config/motors.hpp
@@ -0,0 +1,28 @@
+#ifndef MOTORS_HPP
+#define MOTORS_HPP
+//intagrate into systems files
+#include "okapi/api.hpp"
+namespace Drive{
+  extern okapi::Motor front_left_motor;
+  extern okapi::Motor front_right_motor;
+  extern okapi::Motor back_left_motor;
+  extern okapi::Motor back_right_motor;
+}
+
+namespace Puncher{
+  extern okapi::Motor Motor;
+}
+
+namespace Lift{
+  extern okapi::Motor Motor;
+}
+
+namespace Intake{
+  extern okapi::Motor Motor;
+
+  extern pros::ADILineSensor Puncher;
+  extern pros::ADILineSensor Over;
+  extern pros::ADILineSensor Top;
+  extern pros::ADILineSensor Bottom;
+}
+#endif /* end of include guard: MOTOR_CONFIG_HPP */
