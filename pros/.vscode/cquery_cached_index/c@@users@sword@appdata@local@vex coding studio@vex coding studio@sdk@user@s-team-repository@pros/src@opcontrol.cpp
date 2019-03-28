a59f3cf7e8ac946fb250b6fb2e84fd5d3b38a5a4
swordsparring <swordsparring@gmail.com>
2019-03-28 12:39:13 -0600

    asdf

--- pros/.vscode/cquery_cached_index/c@@users@sword@appdata@local@vex coding studio@vex coding studio@sdk@user@s-team-repository@pros/src@opcontrol.cpp
+++ pros/.vscode/cquery_cached_index/c@@users@sword@appdata@local@vex coding studio@vex coding studio@sdk@user@s-team-repository@pros/src@opcontrol.cpp
@@ -5,6 +5,7 @@
 #include "systems/config/flipper.hpp"
 #include "systems/catapult.hpp"
 #include "systems/config/important/fish_weee.hpp"
+#include "systems/auto/autointake.hpp"
 /**
 * Runs the operator control code. This function will be started in its own task
 * with the default priority and stack size whenever the robot is enabled via
--- pros/src/systems/config/important/fish_weee.cpp
+++ pros/src/systems/config/important/fish_weee.cpp
@@ -1,7 +1,6 @@
 #include "main.h"
 namespace Fishy{
   void fishy(){
-    while (true){
       pros::lcd::set_text(1, "FISH WEEEEEEE");
       pros::lcd::set_text(2, "-_-_-_}(<')-_-_");
       pros::lcd::set_text(3, "-_-_-_-}(<')_-_");
@@ -20,6 +19,6 @@ namespace Fishy{
       pros::delay(500);
       pros::lcd::set_text(2, "-_-_-_}(<')-_-_");
       pros::lcd::set_text(3, "-_-_-_-}(<')-_-_");
-    }
+
   }
 }
